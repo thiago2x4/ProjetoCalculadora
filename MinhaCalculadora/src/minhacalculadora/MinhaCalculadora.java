9d958e4d8258a8f3d676b1c02fc3bca613a3e826
TECNICO NOITE <TECNICO NOITE@LAB204BPC04>
2017-04-26 18:43:07 -0300

    terceira tentativa

--- MinhaCalculadora/src/minhacalculadora/MinhaCalculadora.java
+++ MinhaCalculadora/src/minhacalculadora/MinhaCalculadora.java
@@ -17,7 +17,7 @@ public class MinhaCalculadora {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println();
+        System.out.println("eu estive aqui");
     }
     
 }
